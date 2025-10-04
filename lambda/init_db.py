92c52e2cfe587f765463f34299045e6951989da8
vilasma <vilasma@cybage.com>
2025-10-04 15:00:22 +0530

    updated the template and handlers for kb

--- lambda/init_db.py
+++ lambda/init_db.py
@@ -57,9 +57,18 @@ def lambda_handler(event, context):
     ON document_chunks(document_id);
 
     -- Create GIN index for full-text search
-    CREATE INDEX idx_document_chunks_chunk_text
-    ON document_chunks
-    USING gin (to_tsvector('simple', chunk_text));
+    DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1
+            FROM pg_class c
+            JOIN pg_namespace n ON n.oid = c.relnamespace
+            WHERE c.relname = 'idx_document_chunks_chunk_text'
+        ) THEN
+            CREATE INDEX idx_document_chunks_chunk_text
+            ON document_chunks USING gin (to_tsvector('simple', chunk_text));
+        END IF;
+    END$$;
     """
 
     cur.execute(create_table_query)
